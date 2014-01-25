36570dbabb7c6db4027538f6263c3074d09ab7e8
Librazy <librazy@foxmail.com>
2014-01-25 16:14:43 +0800

    FIX

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
  */
 ?>
 <!DOCTYPE html>
-<html <?php if(is_mobile()){echo 'class="is_mobile" ';} ?> lang="zh-CN">
+<html <?php if(is_mobile()){echo 'class="is_mobile" ';} language_attributes(); ?>>
 <head>
     <meta http-equiv="content-type" content="text/html; charset=<?php bloginfo('charset'); ?>" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
